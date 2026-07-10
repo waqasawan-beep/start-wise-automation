db42576ae629bd6f6417eda0f389af77fb680fe1
Greelgxi-15-s <greelgxi-15-s@192.168.1.85>
2026-07-10 11:36:15 +0500

    Remove verify email dependency from signup test

--- tests/test_homepage.py
+++ tests/test_homepage.py
@@ -2,7 +2,7 @@ import time
 from selenium.webdriver.common.by import By
 from pages.signup_page import SignupPage
 from data import signup_data
-from pages.verify_email_page import VerifyEmailPage
+
 
 def test_open_website(driver):
 
@@ -10,6 +10,7 @@ def test_open_website(driver):
 
     time.sleep(1)
 
+    # Accept cookies
     essentials_button = driver.find_element(
         By.XPATH,
         "//button[text()='Essentials only']"
@@ -18,6 +19,7 @@ def test_open_website(driver):
 
     time.sleep(2)
 
+    # Click login
     login_button = driver.find_element(
         By.XPATH,
         "//a[@href='/login']"
@@ -26,39 +28,36 @@ def test_open_website(driver):
 
     time.sleep(2)
 
+    # Click Sign up
     signup_button = driver.find_element(
         By.XPATH,
         "//button[contains(@class,'text-primary') and normalize-space()='Sign up']"
     )
     signup_button.click()
 
-    time.sleep(1)
+    time.sleep(2)
 
     signup_page = SignupPage(driver)
-
 
     # Insert email
     driver.find_element(
         *signup_page.EMAIL_FIELD
     ).send_keys(signup_data.EMAIL)
 
-
     # Verify email inserted
     assert signup_page.get_email_value() == signup_data.EMAIL
-
 
     # Insert full name
     driver.find_element(
         *signup_page.FULL_NAME_FIELD
     ).send_keys(signup_data.FULL_NAME)
 
-
     # Verify full name inserted
     assert signup_page.get_full_name_value() == signup_data.FULL_NAME
 
     # Insert password
     driver.find_element(
-    *signup_page.PASSWORD_FIELD
+        *signup_page.PASSWORD_FIELD
     ).send_keys(signup_data.PASSWORD)
 
     # Verify password inserted
@@ -71,12 +70,8 @@ def test_open_website(driver):
 
     # Verify confirm password inserted
     assert signup_page.get_confirm_password_value() == signup_data.CONFIRM_PASSWORD
-    # Verify confirm password inserted
-    assert signup_page.get_confirm_password_value() == signup_data.CONFIRM_PASSWORD
+
+    # Click Join Now
     signup_page.click_join_now()
-    verify_email = VerifyEmailPage(driver)
-    assert verify_email.verify_email_page_displayed(), \
-    "Verify email page is not displayed"
 
-
-    time.sleep(20)
+    time.sleep(5)
