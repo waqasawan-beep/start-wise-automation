a71426a41749a67be0f3beb269aa1a9eb8bd647b
Greelgxi-15-s <greelgxi-15-s@192.168.1.85>
2026-07-10 11:40:42 +0500

    Configure Chrome headless mode for CI

--- utils/driver_factory.py
+++ utils/driver_factory.py
@@ -1,13 +1,22 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
 
 
 def create_driver():
-    driver = webdriver.Chrome(
-        service=Service(ChromeDriverManager().install())
-    )
 
-    driver.maximize_window()
+    chrome_options = Options()
+
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument("--disable-gpu")
+    chrome_options.add_argument("--window-size=1920,1080")
+
+    driver = webdriver.Chrome(
+        service=Service(ChromeDriverManager().install()),
+        options=chrome_options
+    )
 
     return driver
